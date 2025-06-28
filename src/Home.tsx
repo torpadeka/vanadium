0fdb74d25c79f5231f2fc8985a8ef007d9acd75a
BuildTools <darrenkent88@gmail.com>
2025-06-28 12:28:06 +0700

    Added react router Data Mode

---
+++ src/Home.tsx
@@ -0,0 +1,18 @@
+import { useQueryCall, useUpdateCall } from "@ic-reactor/react";
+import motokoLogo from "./assets/motoko_moving.png";
+import motokoShadowLogo from "./assets/motoko_shadow.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+
+export default function Home() {
+    const { data: count, refetch } = useQueryCall({
+        functionName: "get",
+    });
+
+    const { call: increment, loading } = useUpdateCall({
+        functionName: "inc",
+        onSuccess: refetch,
+    });
+
+    return <div></div>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import { ActorProvider, AgentProvider } from "@ic-reactor/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { canisterId, idlFactory } from "./declarations/backend";
 import "./index.css";
+import Home from "./Home";
+import Build from "./Build";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <AgentProvider withProcessEnv>
             <ActorProvider idlFactory={idlFactory} canisterId={canisterId}>
-                <App />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/build" element={<Build />} />
+                    </Routes>
+                </BrowserRouter>
             </ActorProvider>
         </AgentProvider>
     </React.StrictMode>
